659ebcaebfd1d9ced8f8aac60ea1d51f55c6d949
hussein <husseiny.1382@gmail.com>
2026-06-07 12:43:06 +0330

    Added two Feature: 1.adding a log area to dashboard for test connection 2.adding a Feature that find all IP in our network

---
+++ Devices/device.py
@@ -0,0 +1,14 @@
+import scapy.all as scapy
+
+### Network Scanning to Find All Devices in our Network ####
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False)[0]
+
+    clients_list = []
+    for element in answered_list:
+        clients_list.append({"ip": element[1].psrc, "mac": element[1].hwsrc})
+    return clients_list
+
--- monitor/views.py
+++ monitor/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from Devices import device
+from Connection import *
 
-# Create your views here.
+### Find devices
+target_ip = "192.168.1.1/24"
+devices = device.scan(target_ip)
+###
 def dashboard_view(request):
-    return render(request,'dashboard.html')
+    # Connection test
+
+    #
+
+    return render(request,'dashboard.html',{"devices":devices})
+
+def settings_view(request):
+    return render(request,"settings/general.html")
+
+
+def alerts_view(request):
+    return render(request,"alerts/list.html")
+
+
+def devices_view(request):
+    return render(request,"devices/detail.html",{"devices":devices})
+
+
+def reports_view(request):
+    return render(request,"reports/detail.html")
